e63891f23c82d4902920e9ae2e4c8bf035463bb7
Citer <citer5@hotmail.com>
2024-09-17 23:40:45 -0700

    .

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,34 +1,54 @@
 <template>
-  <div id="hme_tsk">
-    <div class="tsks">
-      <h2>Tasks</h2>
-      <p>1{{}}</p>
-    </div>
-    <div class="tsks">
-      <h2>Completed</h2>
-      <p>1{{}}</p>
-    </div>
-    <div class="tsks">
-      <h2>Pending</h2>
-      <p>1{{}}</p>
-    </div>
-    <div class="tsks">
-      <h2>In Progress</h2>
-      <p>1{{}}</p>
-    </div>
-  </div>
-  <h1>Good {{}}</h1>
-  <taskslist />
+	<div id="hme_tsk">
+		<div class="tsks">
+			<h2>Tasks</h2>
+			<p>1{{}}</p>
+		</div>
+		<div class="tsks">
+			<h2>Completed</h2>
+			<p>1{{}}</p>
+		</div>
+		<div class="tsks">
+			<h2>Pending</h2>
+			<p>1{{}}</p>
+		</div>
+		<div class="tsks">
+			<h2>In Progress</h2>
+			<p>1{{}}</p>
+		</div>
+	</div>
+	<h1>Good {{ greeting }}</h1>
+	<taskslist />
 </template>
 
 <script>
+import { ref, onMounted } from "vue";
 import Taskslist from "./Taskslist.vue";
 
 export default {
-  name: "Home",
-  components: {
-    Taskslist,
-  },
+	name: "Home",
+	components: {
+		Taskslist,
+	},
+	setup() {
+		const greeting = ref("");
+		const updateSaludo = () => {
+			const hour = new Date().getHours();
+			if (hour > 6 && hour < 12) {
+				greeting.value = "Morning";
+			} else if (hour >= 12 && hour < 18) {
+				greeting.value = "Afternoon";
+			} else {
+				greeting.value = "Night";
+			}
+		};
+		onMounted(() => {
+			updateSaludo();
+		});
+		return {
+			greeting,
+		};
+	},
 };
 </script>
 
--- src/components/Tasks.vue
+++ src/components/Tasks.vue
@@ -1,9 +1,10 @@
 <template>
-	<h1>tasks</h1>
+	<h1>tasks {{ variable }}</h1>
 	<taskslist />
 </template>
 
 <script>
+import apiClient from "@/services/api";
 import Taskslist from "./Taskslist.vue";
 
 export default {
@@ -11,7 +12,31 @@ export default {
 	components: {
 		Taskslist,
 	},
+	data() {
+		return {
+			datos: null,
+			variable: "",
+		};
+	},
+	mounted() {
+		this.fetchData();
+	},
+	methods: {
+		fetchData() {
+			apiClient
+				.get("/variable")
+				.then((response) => {
+					this.datos = response.data;
+					this.variable = response.data.variable;
+				})
+				.catch((error) => {
+					console.error("Hubo un error en la solicitud: ", error);
+				});
+		},
+	},
 };
 </script>
 
-<style></style>
+<style>
+/* Tus estilos aquí */
+</style>
